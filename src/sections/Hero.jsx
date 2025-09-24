d7febc033c3d0c1b0fd2f03b1359e4ca4f55dd9d
Ubada-S <ubadasiddiqicoc@gmail.com>
2025-09-25 00:13:38 +0530

    Adding and positioning the PC 3D model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,47 @@ import { PerspectiveCamera } from "@react-three/drei";
 import HackerRoom from "../components/HackerRoom";
 import CanvasLoader from "../components/CanvasLoader";
 import { Suspense } from "react";
+import { Leva, useControls } from "leva";
 
 const Hero = () => {
+  const x = useControls("HackerRoom", {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+    },
+  });
+
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -18,15 +57,20 @@ const Hero = () => {
       </div>
 
       <div className="w-full h-full absolute inset-0">
+        <Leva />
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
 
             <HackerRoom
-              scale={0.05}
-              position={[0, 0, 0]}
-              rotation={[0, -Math.PI / 2, 0]}
+              // scale={0.07}
+              // position={[0, 0, 0]}
+              position={[2, -8, 2]}
+              rotation={[0, -Math.PI, 0]}
+              scale={0.1}
             />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
       </div>
